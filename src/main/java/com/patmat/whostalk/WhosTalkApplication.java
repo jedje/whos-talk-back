e9dc1e1bb584ea5065abf3dc9acb1f9305864485
jedje <jedje95@gmail.com>
2023-08-05 17:29:39 +0900

    후즈톡 프로젝트 시작하기

---
+++ src/main/java/com/patmat/whostalk/WhosTalkApplication.java
@@ -0,0 +1,13 @@
+package com.patmat.whostalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhosTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhosTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/patmat/whostalk/WhosTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patmat.whostalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhosTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
