4141a8980bcd2b5fb76de0b6bc31e98d7892b6b3
jedje <jedje@aedata.co.kr>
2023-08-07 12:01:07 +0900

    API 테스트를 위한 스웨거 추가

--- src/main/java/com/patmat/whostalk/controller/TestController.java
+++ src/main/java/com/patmat/whostalk/controller/TestController.java
@@ -2,6 +2,9 @@ package com.patmat.whostalk.controller;
 
 import com.patmat.whostalk.service.TestService;
 import com.patmat.whostalk.vo.TestVo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import nonapi.io.github.classgraph.utils.Join;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Api(tags = {"Sample API List"})
 public class TestController {
 
     private final TestService testService;
@@ -18,7 +22,9 @@ public class TestController {
     public TestController(TestService testService) {
         this.testService = testService;
     }
+
     @RequestMapping(method = RequestMethod.GET, path = "/test")
+    @ApiOperation(value = "테스트 API", response = Join.class)
     public List<TestVo> getTestData() {
         return testService.getTestData();
     }
